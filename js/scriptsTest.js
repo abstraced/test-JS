0114e89aff86c6f76e197b3f71cd20768d5847de
abstraced <antoine.luc@gmail.com>
2019-06-23 11:22:28 +0200

    Initial commit

---
+++ js/scriptsTest.js
@@ -0,0 +1,7 @@
+var $container = document.querySelector('.container');
+
+//uncomment the following lines
+
+var button = document.createElement('button');
+ button.innerText = "Test";
+ $container.appendChild(button);
